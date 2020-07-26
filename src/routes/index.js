e969230935dbf373d744155c4b7432e484f51529
Mateusz Hadrys <m.hadrys.95@gmail.com>
2020-07-26 16:12:50 +0200

    add routes

---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+export const routes = {
+  timer: '/',
+  settings: '/settings',
+};
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { routes } from 'routes';
 
 import MainTemplate from 'templates/MainTemplate';
 import SettingsPage from 'views/SettingsPage/SettingsPage';
@@ -8,8 +9,8 @@ import TimerPage from 'views/TimerPage/TimerPage';
 const App = () => (
   <MainTemplate>
     <Switch>
-      <Route path="/" component={TimerPage} exact />
-      <Route path="/settings" component={SettingsPage} exact />
+      <Route path={routes.timer} component={TimerPage} exact />
+      <Route path={routes.settings} component={SettingsPage} exact />
     </Switch>
   </MainTemplate>
 );
